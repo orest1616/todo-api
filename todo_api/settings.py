e61a8d265545bd682b81b228031d02b4242c493d
petrivorest <petrivorest169@gmal.com>
2025-06-05 17:01:40 +0300

    JWT auth with SimpleJWT wired up and working

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -52,7 +52,12 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ]
