a0276a333a2329b31771fae17cfaf883d9d8200b
petrivorest <petrivorest169@gmal.com>
2025-06-05 17:09:42 +0300

    add Task serializers, views and URL routing

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'due_date']
